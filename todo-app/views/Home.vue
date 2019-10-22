0790449c463da1ad5748e4a19b13db2478bfce72
yato <2602231824@qq.com>
2019-10-23 00:24:21 +0800

    删除旧代码，清空

--- todo-app/views/Home.vue
+++ todo-app/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
@@ -16,3 +15,13 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.home {
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+  color: white;
+  text-align: center;
+}
+</style>
