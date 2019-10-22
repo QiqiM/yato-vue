0736402d49a57a0d83ab48a62f8eaa757ea7445a
yato <2602231824@qq.com>
2019-10-23 00:00:58 +0800

    解决因修改项目结构，而不能启动的问题

--- one-div/views/Home.vue
+++ one-div/views/Home.vue
@@ -6,8 +6,9 @@
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// @ is an alias to /src 修改了项目结构后，不能使用
+// @ 了，改用绝对路径，并在当前目录下新建一个package.json文件，做启动文件
+import HelloWorld from '../components/HelloWorld.vue'
 
 export default {
   name: 'home',
