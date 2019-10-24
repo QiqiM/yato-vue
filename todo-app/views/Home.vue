f46068bede1ed5bab9204ac467c0734e87d60831
ytt <2602231824@qq.com>
2019-10-24 19:52:34 +0800

    todo appBar.vue

--- todo-app/views/Home.vue
+++ todo-app/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <app-bar title="TODO"  left="bars" right="search" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '../components/HelloWorld.vue'
+import AppBar from '../components/appBar.vue'
 
 export default {
-  name: 'home',
   components: {
-    HelloWorld
+    AppBar
   }
 }
 </script>
