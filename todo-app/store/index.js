98e754ec5501e6d374819a35378e6581b64c596f
ytt <2602231824@qq.com>
2019-10-25 14:34:29 +0800

    使用require.context()来引入文件

--- todo-app/store/index.js
+++ todo-app/store/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import { tomorrow, today } from '../util'
+import { tomorrow, today } from '../utils/util'
 
 Vue.use(Vuex)
 
--- todo-app/views/Home.vue
+++ todo-app/views/Home.vue
@@ -8,14 +8,31 @@
 
 <script>
 // @ is an alias to /src
-import AppBar from '../components/AppBar.vue'
-import Avatar from '../components/Avatar.vue'
+// import AppBar from '../components/AppBar.vue'
+// import Avatar from '../components/Avatar.vue'
+const path = require('path')
+const files = require.context('../components/home',false, /\.vue$/)
 
+// ["./AppBar.vue","./Avatar.vue"]
+// console.log("==========:%j",files.keys());  
+
+
+const modules = {}
+files.keys().forEach(key => {
+  let name = path.basename(key,'.vue');
+  // console.log("name ",name);  // AppBar | Avatar
+  
+  modules[name] = files(key).default || files(key)
+});
+
+// 使用require.context()来减少重复导入的工作量
 export default {
-  components: {
-    AppBar,
-    Avatar
-  }
+  // components: {
+  //   AppBar,
+  //   Avatar
+  // }
+
+  components: modules
 }
 </script>
 
